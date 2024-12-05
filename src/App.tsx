b231803050bacec1b4f93d5cb9421934ab7dd553
Dmitry Egorov <info@consultapp.ru>
2024-12-05 16:01:25 +0300

    lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
+import styles from "./styles.module.css";
+import { Slider } from "./components/ui/slider";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="counter">
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <label htmlFor="">{count}</label>
-      <button onClick={() => setCount(count + 1)}>+</button>
+    <div>
+      <div className={styles.counter}>
+        <Button onClick={() => setCount(count - 1)}>-</Button>
+        <label htmlFor="">{count}</label>
+        <Button onClick={() => setCount(count + 1)}>+</Button>
+      </div>
+      <Slider
+        defaultValue={[count]}
+        value={[count]}
+        max={100}
+        step={1}
+        onValueChange={(e) => {
+          setCount(e[0]);
+        }}
+      />
     </div>
   );
 }
