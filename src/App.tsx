850ae656b0532a8445b5158682e0ade540b3c8e9
Dmitry Egorov <info@consultapp.ru>
2024-12-19 09:55:19 +0300

    test dark blink fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@ import { theme } from './fixtures/theme'
 import { SurveyComponent } from './components/SurveyComponent'
 import { useElmaData } from './context/ElmaContext/hooks'
 
-function App() {
+function App({ root }: { root: HTMLDivElement | null }) {
   const data = useElmaData()
+
+  const getRootElement = () =>
+    typeof window === 'undefined' || root === null ? undefined : root
+
+  console.log('first', getRootElement())
+
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider
+      // getRootElement={getRootElement}
+      withGlobalClasses={false}
+      defaultColorScheme="light"
+      forceColorScheme="light"
+      // cssVariablesSelector={`#${root?.id}`}
+      theme={theme}
+    >
       <SurveyComponent questions={data} key={`SurveyComponent`} />
     </MantineProvider>
   )
