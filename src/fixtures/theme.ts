3339cb0779bac4b4420eb8bc436ee9192a3c5a5c
Dmitry Egorov <info@consultapp.ru>
2024-12-20 16:29:00 +0300

    minor improvements

--- src/fixtures/theme.ts
+++ src/fixtures/theme.ts
@@ -1,21 +1,21 @@
 import { createTheme } from '@mantine/core'
 import { generateColors } from '@mantine/colors-generator'
 
+const ff = window
+  .getComputedStyle(document.body, null)
+  .getPropertyValue('font-family')
+
+const baseColor = window
+  .getComputedStyle(document.body, null)
+  .getPropertyValue('--color-theme-900')
+
 export const theme = createTheme({
-  fontFamily:
-    window
-      .getComputedStyle(document.body, null)
-      .getPropertyValue('font-family') ??
-    '"SF Pro Display", Helvetica, Arial, sans-serif',
+  fontFamily: ff ? ff : '"SF Pro Display", Helvetica, Arial, sans-serif',
   defaultRadius: 'md',
   scale: 1.6,
   primaryColor: 'elma-colors',
   colors: {
-    'elma-colors': generateColors(
-      window
-        .getComputedStyle(document.body, null)
-        .getPropertyValue('--color-theme-900') ?? '#000000'
-    ),
+    'elma-colors': generateColors(baseColor ? baseColor : '#062543'),
   },
 })
 
