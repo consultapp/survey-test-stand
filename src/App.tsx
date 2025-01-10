95c033848f5fa26e10173918b709fb6475d5d54c
Dmitry Egorov <info@consultapp.ru>
2025-01-10 12:37:42 +0300

    -- local styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { MantineProvider } from '@mantine/core'
 import { theme } from './fixtures/theme'
 import { SurveyComponent } from './components/SurveyComponent'
 import { useElmaData } from './context/ElmaContext/hooks'
-import { MantineRootStyleComponent } from './components/MantineRootStyleComponent'
 import { useCallback } from 'react'
+import './styles.css'
 
 function App({ root }: { root: HTMLDivElement | null }) {
   const data = useElmaData()
@@ -16,18 +16,13 @@ function App({ root }: { root: HTMLDivElement | null }) {
   console.log('root:', getRootElement())
 
   return (
-    <MantineRootStyleComponent>
-      <MantineProvider
-        getRootElement={getRootElement}
-        cssVariablesSelector={`#${root?.id}`}
-        withGlobalClasses={false}
-        defaultColorScheme="light"
-        forceColorScheme="light"
-        theme={theme}
-      >
-        <SurveyComponent questions={data} key={`SurveyComponent`} />
-      </MantineProvider>
-    </MantineRootStyleComponent>
+    <MantineProvider
+      defaultColorScheme="light"
+      forceColorScheme="light"
+      theme={theme}
+    >
+      <SurveyComponent questions={data} key={`SurveyComponent`} />
+    </MantineProvider>
   )
 }
 
