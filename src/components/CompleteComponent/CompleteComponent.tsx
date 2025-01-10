2cc21e2116f78b6ac73a5c35ef6695e205597359
Dmitry Egorov <info@consultapp.ru>
2025-01-10 21:13:16 +0300

    total errors

--- src/components/CompleteComponent/CompleteComponent.tsx
+++ src/components/CompleteComponent/CompleteComponent.tsx
@@ -1,23 +1,49 @@
 import { Flex, Text } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { Modal, Button } from '@mantine/core'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useElmaCompleteCallback } from '@/context/ElmaContext/hooks'
 import { log } from '@/utils'
+import {
+  useStatusContext,
+  useStatusContextDispatch,
+} from '@/context/StatusContext/hooks'
+import { Status } from '@/fixtures/status'
 
 type Props = { root: HTMLDivElement | null }
 
 export const CompleteComponent = ({ root }: Props) => {
   const [opened, { open, close }] = useDisclosure(false)
   const completeHandler = useElmaCompleteCallback()
+  const statuses = useStatusContext()
+  const dispatch = useStatusContextDispatch()
 
-  const submitHandler = () => {
+  const submitHandler = useCallback(() => {
     log('Completed')
     completeHandler()
     close()
-  }
+  }, [close, completeHandler])
 
-  const fireTryCompleteEventTestBtn = () => {
+  const rejectHandler = useCallback(() => {
+    close()
+  }, [close])
+
+  const checkStatuses = useCallback(() => {
+    Object.entries(statuses).forEach(([k, v]) => {
+      if (v === Status.idle) dispatch({ type: Status.empty, payload: k })
+    })
+  }, [dispatch, statuses])
+
+  const countErrors = useCallback(
+    () =>
+      Object.values(statuses).reduce(
+        (acc, val) => acc + (val !== Status.approved ? 1 : 0),
+        0
+      ),
+    [statuses]
+  )
+
+  const fireTryCompleteEventTestBtn = useCallback(() => {
     log('TryCompleteEvent dispatched')
     if (root)
       root.dispatchEvent(
@@ -27,7 +53,7 @@ export const CompleteComponent = ({ root }: Props) => {
           },
         })
       )
-  }
+  }, [root])
 
   useEffect(() => {
     const currentController = new AbortController()
@@ -37,6 +63,7 @@ export const CompleteComponent = ({ root }: Props) => {
         () => {
           log('TryCompleteEvent caught')
           open()
+          checkStatuses()
         },
         {
           signal: currentController.signal,
@@ -44,22 +71,28 @@ export const CompleteComponent = ({ root }: Props) => {
       )
 
     return () => currentController.abort()
-  }, [open, root])
+  }, [checkStatuses, open, root])
+
+  const errorCount = countErrors()
 
   return (
     <>
       <Modal
         opened={opened}
         onClose={close}
-        title="Подтверждение теста"
+        title="Завершить незаконченный тест?"
         centered
         zIndex={1060}
       >
-        <Flex direction="column" gap="md">
-          <Text>Завершить тест?</Text>
-          <Flex gap="md">
+        <Flex direction="column" gap="xs">
+          {errorCount && (
+            <Text>{`Незаполненных вопросов: ${errorCount} шт.`}</Text>
+          )}
+          <Flex gap="xs">
             <Button onClick={submitHandler}>Да</Button>
-            <Button>Нет</Button>
+            <Button onClick={rejectHandler} variant="outline">
+              Нет
+            </Button>
           </Flex>
         </Flex>
       </Modal>
