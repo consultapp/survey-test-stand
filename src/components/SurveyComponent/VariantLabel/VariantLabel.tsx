a2b1466f581d0209c51c83090517d3f6b46ac2ad
Dmitry Egorov <info@consultapp.ru>
2025-05-01 13:19:51 +0300

    tiles update 1

--- src/components/SurveyComponent/VariantLabel/VariantLabel.tsx
+++ src/components/SurveyComponent/VariantLabel/VariantLabel.tsx
@@ -1,37 +1,42 @@
 import { Flex, Text } from '@mantine/core'
+import { memo, useMemo } from 'react'
 
 type Props = { label: string; isChecked: boolean }
 
-export const VariantLabel = ({ label, isChecked }: Props) => {
+export const VariantLabel = memo(({ label, isChecked }: Props) => {
+  const style = useMemo(
+    () => ({
+      cursor: 'pointer',
+      minHeight: '100px',
+      padding: 16,
+      border: `2px solid ${
+        isChecked
+          ? 'var(--mantine-primary-color-filled)'
+          : 'var(--mantine-color-gray-4)'
+      }`,
+      borderRadius: '8px',
+      backgroundColor: isChecked
+        ? 'var(--mantine-primary-color-light)'
+        : 'transparent',
+      '&:hover': {
+        backgroundColor: 'var(--mantine-color-gray-1)',
+      },
+      transition: 'all 0.3s ease',
+      '&[dataChecked]': {
+        borderWidth: '3px',
+        transform: 'scale(0.98)',
+      },
+      '&:active': {
+        transform: 'scale(0.96)',
+      },
+      // justifyContent: 'center',
+      alignItems: 'center',
+    }),
+    [isChecked]
+  )
+
   return (
-    <Flex
-      gap="sm"
-      style={{
-        cursor: 'pointer',
-        minHeight: '100px',
-        padding: 16,
-        border: `2px solid ${
-          isChecked
-            ? 'var(--mantine-primary-color-filled)'
-            : 'var(--mantine-color-gray-4)'
-        }`,
-        borderRadius: '8px',
-        backgroundColor: isChecked
-          ? 'var(--mantine-primary-color-light)'
-          : 'transparent',
-        '&:hover': {
-          backgroundColor: 'var(--mantine-color-gray-1)',
-        },
-        transition: 'all 0.3s ease',
-        '&[data-checked]': {
-          borderWidth: '3px',
-          transform: 'scale(0.98)',
-        },
-        '&:active': {
-          transform: 'scale(0.96)',
-        },
-      }}
-    >
+    <Flex gap="xs" style={style}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="20"
@@ -44,16 +49,17 @@ export const VariantLabel = ({ label, isChecked }: Props) => {
         stroke-linejoin="round"
         style={{
           opacity: isChecked ? 1 : 0,
-          transition: 'opacity 0.2s ease',
+          transition: 'opacity 0.3s ease',
           color: 'var(--mantine-primary-color-filled)',
         }}
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M5 12l5 5l10 -10" />
       </svg>
+
       <Text fw={500} size="md">
         {label}
       </Text>
     </Flex>
   )
-}
+})
