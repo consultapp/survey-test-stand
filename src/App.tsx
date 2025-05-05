9cb7a5d023dd06181acfc44660bbcdfd45947969
Dmitry Egorov <info@consultapp.ru>
2025-05-05 19:59:50 +0300

    -- test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { Button, MantineProvider, Stack } from '@mantine/core'
+import { MantineProvider, Stack } from '@mantine/core'
 import { theme } from './fixtures/theme'
 import { SurveyComponent } from './components/SurveyComponent'
 import { useElmaData } from './context/ElmaContext/hooks'
 import CompleteComponent from './components/CompleteComponent/CompleteComponent'
 import './styles.css'
 import { TimerComponent } from './components/TimerComponent/TimerComponent'
-import { useState } from 'react'
 
 function App({ root }: { root: HTMLDivElement | null }) {
   const data = useElmaData()
-  const [s, ss] = useState(true)
 
   return (
     <MantineProvider
@@ -18,8 +16,7 @@ function App({ root }: { root: HTMLDivElement | null }) {
       theme={theme}
     >
       <Stack>
-        <Button onClick={() => ss(!s)}>Hide</Button>
-        {s && <TimerComponent />}
+        <TimerComponent />
         <SurveyComponent questions={data} key={`SurveyComponent`} />
         <CompleteComponent root={root} />
       </Stack>
