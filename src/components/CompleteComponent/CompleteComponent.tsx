855a1192b37495e2ebae971d5f5a88f1549cf4a1
Dmitry Egorov <info@consultapp.ru>
2025-05-12 15:55:40 +0300

    rejectHandler fix

--- src/components/CompleteComponent/CompleteComponent.tsx
+++ src/components/CompleteComponent/CompleteComponent.tsx
@@ -105,7 +105,7 @@ export const CompleteComponent = ({ root }: Props) => {
     <>
       <Modal
         opened={opened}
-        onClose={close}
+        onClose={rejectHandler}
         title="Нельзя завершить незаконченный тест"
         centered
         zIndex={1060}
